b69c8bb3b584011368e7e17eff7830c5d1f850d7
alice102728 <79676852+alice102728@users.noreply.github.com>
2021-04-09 10:42:33 +0800

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+char [100][10]
+int mian()
+{
+	int n;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++){
+		scanf("%s",&a[i]);
+	}
+	for(int i=0;i<n;i++){
+		for(int j=i+1;j<n;j++){
+			if(strcmp(a[i],a[j]);
+			strcpy(temp,a[i]);
+			strcpy(a[j],a[i]);
+			strcpy(a[j],temp);
+			}
+	}
+	for(int i=0;i<n;i++){
+	printf("%s",a[i]);
+	}
+}
