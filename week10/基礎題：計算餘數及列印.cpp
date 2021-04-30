36da810a0f340b0bc28f9ec6dff82c33ff819234
alice102728 <79676852+alice102728@users.noreply.github.com>
2021-04-30 15:50:47 +0800

    Add files via upload

---
+++ week10/基礎題：計算餘數及列印.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+	int x,y;
+	scanf("%d%d",&x,&y);
+	printf("Enter two numbers: The remainder is %d\n",x%y);
+}
---
+++ week10/進階題：字串長度.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char a[100],b[100];
+	scanf("%s%s",&a,&b);
+	int lena=strlen(a),lenb=strlen(b);
+	if(lena>lenb) printf("1");
+	else if(lena<lenb) printf("-1");
+	else{
+		printf("%d",strcmp(a,b));
+	}
+}
