0da80ed608329fe5050193a7dfd43515a18187ce
alice102728 <79676852+alice102728@users.noreply.github.com>
2021-05-14 16:16:15 +0800

    Add files via upload

---
+++ week12/基礎題：三數最小.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int min(int a,int b,int c)
+{
+	int ans=a;
+	if(ans>b) ans=b;
+	if(ans>c) ans=c;
+	return ans;
+}
+int main()
+{
+	int x,y,z;
+	scanf("%d%d%d",&x,&y,&z);
+	printf("%d\n",min(x,y,z));
+}
